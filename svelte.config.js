403cda59879c8ce316dd1e3dee71174d683ac4ea
Raspberry1111 <raspberry1111@duck.com>
2026-02-16 16:17:47 -0500

    Change some stuff

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,13 @@ import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
-    },
-  },
+	kit: {
+		adapter: adapter(),
+		paths: {
+			//   base: process.env.NODE_ENV === "production" ? "/" : "",
+			base: ""
+		},
+	},
 };
 
 export default config;
